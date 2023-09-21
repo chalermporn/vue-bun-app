5bfcb832ecdcd6e0f4e210a734b3a526d5292199
Chalermporn Posoppitakwong <bird.urights@gmail.com>
2023-09-21 21:19:43 +0700

    feat: update config env and vite.comfig.ts

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,24 @@
 /// <reference types="vite/client" />
+
+
+interface ImportMetaEnv {
+    readonly SITE_TITLE: string;
+    readonly VITE_APP_VERSION: string;
+    readonly NODE_ENV: string;
+    readonly VITE_APP_PATH: string;
+    readonly VITE_APP_GATEWAY: string;
+    readonly VITE_APP_TITLE: string;
+    readonly VITE_APP_IDLE_TIME: string;
+    readonly VITE_THAI_ID_PATH: string;
+    readonly VITE_APP_SSO_REALM: string;
+    readonly VITE_APP_SSO_URL: string;
+    readonly VITE_APP_CLIENT_ID: string;
+    readonly VITE_APP_HOST_NAME: string;
+    readonly VITE_APP_HOST_NAME_PORT: string;
+    
+  }
+  
+  interface ImportMeta {
+    readonly env: ImportMetaEnv
+  }
+  
--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,53 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+const VITE_APP_HOST_NAME = process.env.VITE_APP_HOST_NAME
+const VITE_APP_HOST_NAME_PORT = process.env.VITE_APP_HOST_NAME_PORT
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
   ],
+  server: {
+    https: false,
+    host: VITE_APP_HOST_NAME,
+    port: Number(VITE_APP_HOST_NAME_PORT),
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
+
+
+// import { fileURLToPath, URL } from 'node:url'
+
+// import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// import vue from '@vitejs/plugin-vue'
+// import { defineConfig, loadEnv } from 'vite'
+// export default defineConfig(({ mode }) => {
+//   const {
+//     VITE_APP_HOST_NAME,
+//     VITE_APP_HOST_NAME_PORT,
+//   } = loadEnv(mode, process.cwd(), '')
+//   return {
+//     plugins: [
+//       vue(),
+//       vueJsx(),
+//     ],
+//     server: {
+//       https: false,
+//       host: VITE_APP_HOST_NAME,
+//       port: Number(VITE_APP_HOST_NAME_PORT),
+//     },
+//     resolve: {
+//       alias: {
+//         '@': fileURLToPath(new URL('./src', import.meta.url)),
+//       },
+//     }
+//   }
+// })
